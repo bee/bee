47321f817c5a6c05e030f21d8ebad6bf4a8e4d19
Marius Tolzmann <tolzmann@molgen.mpg.de>
2010-09-26 00:33:44 +0000

    bee-check/bee-install/beeversion: added gpl header

--- src/beeinstall.sh
+++ src/beeinstall.sh
@@ -1,8 +1,23 @@
 #!/bin/bash
-
-###############################################################################
-###############################################################################
-###############################################################################
+#
+# bee-install - install a bee-pkg
+# Copyright (C) 2009-2010 
+#       Tobias Dreyer <dreyer@molgen.mpg.de>
+#       Marius Tolzmann <tolzmann@molgen.mpg.de>
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
 
 VERSION=0.1
 
@@ -18,7 +33,7 @@ fi
 
 ##### iee_usage ###############################################################
 iee_usage() {
-    echo "bee_install v${VERSION} 2009-2010"
+    echo "bee-install v${VERSION} 2009-2010"
     echo ""
     echo "  by Tobias Dreyer and Marius Tolzmann <{dreyer,tolzmann}@molgen.mpg.de>"
     echo ""
--- src/beeversion/beeversion.c
+++ src/beeversion/beeversion.c
@@ -1,20 +1,21 @@
 /*
-    beeversion - compare bee package versionnumbers
-    Copyright (C) 2010  David Fessler <dfessler@uni-potsdam.de>
-                        Marius Tolzmann <tolzmann@molgen.mpg.de>
-
-    This program is free software; you can redistribute it and/or modify
-    it under the terms of the GNU General Public License as published by
-    the Free Software Foundation; either version 3 of the License, or
-    (at your option) any later version.
-
-    This program is distributed in the hope that it will be useful,
-    but WITHOUT ANY WARRANTY; without even the implied warranty of
-    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
-    GNU General Public License for more details.
-
-    You should have received a copy of the GNU General Public License
-    along with this program; if not, see <http://www.gnu.org/licenses/>.
+** beeversion - compare bee package versionnumbers
+** Copyright (C) 2010 
+**       Marius Tolzmann <tolzmann@molgen.mpg.de>
+**       David Fessler <dfessler@uni-potsdam.de>
+**
+** This program is free software; you can redistribute it and/or modify
+** it under the terms of the GNU General Public License as published by
+** the Free Software Foundation; either version 3 of the License, or
+** (at your option) any later version.
+**
+** This program is distributed in the hope that it will be useful,
+** but WITHOUT ANY WARRANTY; without even the implied warranty of
+** MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+** GNU General Public License for more details.
+**
+** You should have received a copy of the GNU General Public License
+** along with this program; if not, see <http://www.gnu.org/licenses/>.
 */
 
 #include <stdio.h>
@@ -22,7 +23,6 @@
 #include <getopt.h>
 #include <string.h>
 #include <ctype.h>
-
 
 #define EXTRA_UNKNOWN 200
 #define EXTRA_ALPHA   1
