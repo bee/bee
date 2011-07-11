d51375d846fbba88949c87350ad1ba721b95829d
Marius Tolzmann <tolzmann@molgen.mpg.de>
2011-07-11 19:26:52 +0200

    beesort: first revision

---
+++ src/beeversion/beesort.c
@@ -0,0 +1,76 @@
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <assert.h>
+
+#include "beeversion.h"
+#include "compare.h"
+#include "parse.h"
+#include "tree.h"
+
+void my_free_data(void *data)
+{
+    struct beeversion *v = data;
+
+    free(v->string);
+    free(v);
+}
+
+int my_compare_key(void *a, void *b)
+{
+    return compare_beepackages(a,b);
+}
+
+void my_print_key(void *key)
+{
+    printf("%s", ((struct beeversion *)key)->string);
+}
+
+struct tree *init_tree(void)
+{
+    struct tree *tree;
+
+    tree = tree_allocate();
+
+    assert(tree);
+
+    tree->free_data   = &my_free_data;
+    tree->compare_key = &my_compare_key;
+    tree->print_key   = &my_print_key;
+
+    return tree;
+}
+
+
+int main(int argc, char *argv[])
+{
+    struct tree *tree;
+    
+    struct beeversion *v;
+
+    int i = 1;
+
+    tree = init_tree();
+
+    while (i < argc) {
+        v = calloc(1, sizeof(*v));
+        assert(v);
+    
+        if (argv[i][0] == '-') {
+            parse_version(&argv[i][1], v);
+            tree_delete(tree, v);
+        } else {
+            parse_version(argv[i], v);
+            tree_insert(tree, v);
+        }
+        i++;
+
+    }
+    tree_print(tree);
+
+    tree_free(tree);
+
+    return 0;
+}
+
