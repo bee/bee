ce8ac7117ed23618cca616231ad71a9b60bcc27d
Matthias Ruester <ruester@molgen.mpg.de>
2012-08-31 10:45:39 +0200

    contrib: update bash completion script

--- contrib/bee-completion.sh
+++ contrib/bee-completion.sh
@@ -7,22 +7,16 @@ _bee_completion()
     cur="${COMP_WORDS[COMP_CWORD]}"
     prev="${COMP_WORDS[COMP_CWORD-1]}"
 
-    bee_tools="check dep download init install list query remove"
-    bee_dep_options="rebuild update remove list conflicts"
+    bee_tools="cache check download init install list query remove update"
 
     if [ "${prev}" = "bee" ]; then
         COMPREPLY=( $(compgen -W "${bee_tools}" -- ${cur} ) )
         return 0
     fi
 
-    if [ "${prev}" = "dep" ]; then
-        COMPREPLY=( $(compgen -W "${bee_dep_options}" -- ${cur}) )
-        return 0
-    fi
-
     packages="$(bee list -a)"
-    packages="${packages} $(bee list -i)"
     COMPREPLY=( $(compgen -W "${packages}" -- ${cur}) )
+
     return 0
 }
 
